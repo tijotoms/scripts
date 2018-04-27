9ba0d0bf5949180af05e41ebde98e8418bf5a712
sreejith <sreejith.ms@perfomatix.com>
2018-04-27 11:51:26 +0530

    LP-chnges

--- backup.sh
+++ backup.sh
@@ -2,10 +2,10 @@
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 cd /opt/backups
 file=$(date +"%Y%m%d")
-mongodump -h ds161148.mlab.com --port 61148 -d loyalty-studios -u backup_loyaltyprod -p G2X3Q7xwsdcvCtXkl8q --out /opt/backups/${file}
+mongodump -h <hostname> --port 61148 -d <database> -u <user> -p <password> --out /opt/backups/${file}
 if [ "${?}" -eq 0 ]; then
   tar -cvzf ${file}.gz ${file}
-  aws s3 cp ${file}.gz s3://backup-loyaltyproduction/
+  aws s3 cp ${file}.gz s3://<s3 bucketname>/
   rm /opt/backups/${file}.gz
 else
   echo "Error backing"
